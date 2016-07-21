0d4173f12bb66a7535608f437e965f773aeb73e0
Shadorc <bi-crosseur@hotmail.fr>
2016-07-21 17:00:01 +0200

    Update Package Hierarchy

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-h3. What is this?
+<h1>What is this ?</h1>
 
-This is a speech recognition application built on sphinx4 (http://cmusphinx.sourceforge.net/sphinx4/)
+This is a Java speech recognition application built on sphinx4 (http://cmusphinx.sourceforge.net/sphinx4/)
--- src/me/shadorc/voice_recognition/Main.java
+++ src/me/shadorc/voice_recognition/Main.java
@@ -1,16 +1,4 @@
-/*
- * Copyright 1999-2004 Carnegie Mellon University.
- * Portions Copyright 2004 Sun Microsystems, Inc.
- * Portions Copyright 2004 Mitsubishi Electric Research Laboratories.
- * All Rights Reserved.  Use is subject to license terms.
- *
- * See the file "license.terms" for information on usage and
- * redistribution of this file, and for a DISCLAIMER OF ALL
- * WARRANTIES.
- *
- */
-
-package flat10c.flat_voice_commands;
+package me.shadorc.voice_recognition;
 
 import edu.cmu.sphinx.frontend.util.Microphone;
 import edu.cmu.sphinx.recognizer.Recognizer;
@@ -20,13 +8,13 @@ import edu.cmu.sphinx.result.Path;
 import edu.cmu.sphinx.result.Result;
 import edu.cmu.sphinx.util.props.ConfigurationManager;
 
-public class FlatVoiceCommands {
+public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Loading config ...");
 
 		/* Load Sphinx Configuration */
-		ConfigurationManager cm = new ConfigurationManager(FlatVoiceCommands.class.getResource("sphinx.config.xml"));
+		ConfigurationManager cm = new ConfigurationManager(Main.class.getResource("sphinx.config.xml"));
 
 		System.out.println("Initializing Sphinx ...");
 
@@ -35,7 +23,7 @@ public class FlatVoiceCommands {
 
 		System.out.println("Starting Microphone ...");
 
-		// start the microphone or exit if this is not possible
+		/* Start the microphone or exit if this is not possible */
 		Microphone microphone = (Microphone) cm.lookup("microphone");
 		if(!microphone.startRecording()) {
 			System.out.println("Cannot start microphone.");
@@ -46,7 +34,7 @@ public class FlatVoiceCommands {
 		System.out.println("Say a command to the flat.");
 		System.out.println("Start speaking. Press Ctrl-C to quit.\n");
 
-		// Loop the recognition until the program exits.
+		/* Loop the recognition until the program exits. */
 		while (true) {
 
 			Result result = recognizer.recognize();
